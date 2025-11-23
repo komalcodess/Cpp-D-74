e81b5932f7ade722e7320c9deb41082610af381e
komalcodess <komalkomss02@gmail.com>
2025-11-23 23:43:43 +0530

    Add files via upload

---
+++ D 74 sum of the series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+long long fact(int a){
+    long long f=1;
+for(int i=a;i>0;i--){
+    f*=i;
+    }
+    return f;
+}
+
+
+float series(int n){
+float sum=0.0;
+for(int i=1;i<=n;i++){
+    int den=(i*i)+1;
+    sum=sum+(float)i/fact(den);
+}
+return sum;
+}
+
+int main(){
+int n;
+printf("Enter the number of term    : ");
+scanf("%d",&n);
+printf("The sum of the series upto %d terms is: %f",n,series(n));
+return 0;
+}
